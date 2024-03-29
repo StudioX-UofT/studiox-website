96b020470cfbe5a04e1b97c4320b5eb985c7fe2e
m2zhang <marilyn2zhang@gmail.com>
2024-03-29 18:09:32 -0400

    See Project Button

---
+++ src/components/SeeProjects/SeeProjBtn.tsx
@@ -0,0 +1,15 @@
+import SeeProj from './see_proj.svg';
+import "./SeeProj.css";
+
+
+
+const SeeProjectBtn = () => {
+    return (
+        <div className="svg-container">
+            <img src={ SeeProj } />
+            {/* <a href = > */}
+        </div>
+    )
+}
+
+export default SeeProjectBtn
